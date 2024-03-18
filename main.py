ca70a77e081395ee8799441e297344a79b250d47
anoopkashyap07 <55287654+anoopkashyap07@users.noreply.github.com>
2024-03-18 13:22:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 # Set up YouTube API service
 DEVELOPER_KEY = os.environ.get("DEVELOPER_KEY")
-youtube = build("youtube", "v3", developerKey="YOUR_DEVELOPER_KEY")
+youtube = build("youtube", "v3", developerKey="DEVELOPER_KEY")
 
 def get_channel_id(channel_name):
     search_response = youtube.search().list(
